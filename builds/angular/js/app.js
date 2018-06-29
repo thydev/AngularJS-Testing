f045237251895754f5bb220dd6c7d76e2cd59258
thydev <nginchanthy@gmail.com>
2018-06-28 18:20:58 -0700

    route

--- builds/angular/js/app.js
+++ builds/angular/js/app.js
@@ -1,17 +1,16 @@
-var myApp = angular.module('myApp', [
-  'ngRoute',
-  'myControllers'
+var myApp = angular.module('myApp', ['ngRoute', 'myControllers']);
+
+myApp.config([
+  '$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'js/partials/search.html',
+        controller: 'SearchController'
+      })
+      .when('/details/:itemId', {
+        templateUrl: 'js/partials/details.html',
+        controller: 'DetailsController'
+      });
+  }
 ]);
-
-myApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'js/partials/search.html',
-      controller: 'SearchController'
-    })
-    .when('/details/:itemId', {
-      templateUrl: 'js/partials/details.html',
-      controller: 'DetailsController'
-    });
-}]);
-
--- builds/angular/js/controllers.js
+++ builds/angular/js/controllers.js
@@ -1,15 +1,20 @@
 var myControllers = angular.module('myControllers', []);
 
-myControllers.controller('SearchController', 
-  function MyController($scope, $http) {
-    $http.get('js/data.json').then(function(response) {
-      $scope.artists = response.data;
-      $scope.artistOrder = 'name';
+myControllers.controller('SearchController', function MyController(
+  $scope,
+  $http
+) {
+  $http.get('js/data.json').then(function(response) {
+    $scope.artists = response.data;
+    $scope.artistOrder = 'name';
   });
 });
 
-myControllers.controller('DetailsController', 
-function MyController($scope, $http, $routeParams) {
+myControllers.controller('DetailsController', function MyController(
+  $scope,
+  $http,
+  $routeParams
+) {
   $http.get('js/data.json').then(function(response) {
     $scope.artists = response.data;
     $scope.whichItem = $routeParams.itemId;
@@ -20,11 +25,10 @@ function MyController($scope, $http, $routeParams) {
       $scope.prevItem = $scope.artists.length - 1;
     }
 
-    if ($routeParams.itemId < $scope.artists.length-1) {
+    if ($routeParams.itemId < $scope.artists.length - 1) {
       $scope.nextItem = Number($routeParams.itemId) + 1;
     } else {
       $scope.nextItem = 0;
     }
-
-});
+  });
 });
